457b454d57329dcbc6be57b19662a1ddf7b5584d
selistym <selistym@protonmail.com>
2019-02-25 03:36:05 -0800

    add barchart

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { Button, Section, Box } from 'react-bulma-components';
+import StackChart from './components/StackChart';
 import './App.css';
 
 class App extends Component {
+  state = {
+    data: [12, 5, 6, 6, 9, 10],
+    width: 700,
+    height: 500,
+    id: "root"
+  };
   render() {
     return (
       <div className="App">
-        <Section>
+        <Section>        
           <Box>
-            <Button color="primary">My Bulma button</Button>            
+            <StackChart data={this.state.data} width={this.state.width} height={this.state.height} />
+            <Button color="primary">My Bulma button</Button>
           </Box>    
         </Section>
       </div>
